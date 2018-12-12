fb85a7f8f33818964da29135b395bca3377d6355
Jordan Savant <md5madman@gmail.com>
2018-12-12 06:55:03 -0600

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+## Installation
+
+- Install python 3
+- Install pygame `pip install -U pygame --user`
---
+++ main_pygame.py
@@ -0,0 +1,26 @@
+import sys, pygame
+
+pygame.init()
+
+size = width, height = 640, 480
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
