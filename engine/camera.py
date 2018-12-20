6036bd1c5ad64a39ac6d2b9f4c39783cf207230b
Jordan Savant <md5madman@gmail.com>
2018-12-20 14:21:29 -0600

    camera class

---
+++ engine/camera.py
@@ -0,0 +1,89 @@
+import math
+
+class Camera(object):
+
+    def __init__(self):
+        self.worldX = 0
+        self.worldY = 0
+        self.angle = 0
+
+    def fncross(self, x1, y1, x2, y2):
+        return x1 * y2 - y1 * x2
+
+    def intersect(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        x = self.fncross(x1, y1, x2, y2)
+        y = self.fncross(x3, y3, x4, y4)
+        det = self.fncross(x1 - x2, y1 - y2, x3 - x4, y3 - y4)
+        x = self.fncross(x, x1 - x2, y, x3 - x4) / det
+        y = self.fncross(x, y1 - y2, y, y3 - y4) / det
+        return [x, y]
+
+    def transformWall(self, lineDef):
+
+        # Transform vertices relative to player
+        tx1 = lineDef.start[0] - self.worldX
+        ty1 = lineDef.start[1] - self.worldY
+        tx2 = lineDef.end[0] - self.worldX
+        ty2 = lineDef.end[1] - self.worldY
+
+        # Rotate them around the players view
+        tz1 = tx1 * math.cos(self.angle) + ty1 * math.sin(self.angle)
+        tz2 = tx2 * math.cos(self.angle) + ty2 * math.sin(self.angle)
+        tx1 = tx1 * math.sin(self.angle) - ty1 * math.cos(self.angle)
+        tx2 = tx2 * math.sin(self.angle) - ty2 * math.cos(self.angle)
+
+        return (tx1, ty1, tz1, tx2, ty2, tz2)
+
+    def projectWall(self, lineDef):
+
+        # PROJECTED
+        (tx1, ty1, tz1, tx2, ty2, tz2) = self.transformWall(lineDef)
+
+        # PERSPECTIVE TRANSFORMED
+
+        # Clip
+        # determine clipping, if both z's < 0 its totally behind
+        # if only 1 is negative it can be clipped
+        if tz1 > 0 or tz2 > 0:
+            # if line crosses the players view plane clip it
+            # i think the last two are set by refs
+            i1 = self.intersect(tx1, tz1, tx2, tz2, -0.0001, 0.0001, -20, 5)
+            ix1 = i1[0]
+            iz1 = i1[1]
+            i2 = self.intersect(tx1, tz1, tx2, tz2, 0.0001, 0.0001, 20, 5)
+            ix2 = i2[0]
+            iz2 = i2[1]
+            if tz1 <= 0:
+                if iz1 > 0:
+                    tx1 = ix1
+                    tz1 = iz1
+                else:
+                    tx1 = ix2
+                    tz1 = iz2
+            if tz2 <= 0:
+                if iz1 > 0:
+                    tx2 = ix1
+                    tz2 = iz1
+                else:
+                    tx2 = ix2
+                    tz2 = iz2
+
+        # If not behind us
+        if (tz1 > 0 and tz2 > 0):
+            # Transform
+            x1 = -tx1 * 16 / tz1
+            y1a = -50 / tz1
+            y1b = 50 / tz1
+            x2 = -tx2 * 16 / tz2
+            y2a = -50 / tz2
+            y2b = 50 / tz2
+
+            # Render
+            topLine = [[50 + x1, 50 + y1a], [50 + x2, 50 + y2a]]
+            bottomLine = [[50 + x1, 50 + y1b], [50 + x2, 50 + y2b]]
+            leftLine = [[50 + x1, 50 + y1a], [50 + x1, 50 + y1b]]
+            rightLine = [[50 + x2, 50 + y2a], [50 + x2, 50 + y2b]]
+
+            return (topLine, rightLine, bottomLine, leftLine)
+        
+        return (None, None, None, None)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from engine.display import Display
 from engine.eventlistener import EventListener
 from engine.linedef import LineDef
 from engine.solidbspnode import SolidBSPNode
+from engine.camera import Camera
 
 print("\n")
 
@@ -67,9 +68,13 @@ solidBsp = SolidBSPNode(allLineDefs)
 
 # TESTING WALL DRAWING
 wallTest = allLineDefs[4]
-camPoint = [90, 150]
-camDirRads = 0
-camDir = engine.mathdef.toVector(camDirRads)
+# camPoint = [90, 150]
+# camDirRads = 0
+# camDir = engine.mathdef.toVector(camDirRads)
+camera = Camera()
+camera.worldX = 90
+camera.worldY = 150
+camera.angle = 0
 
 
 # testPoint = [60, 20]
@@ -94,35 +99,46 @@ def mode_down():
     global mode
     mode = (mode - 1) % max_modes
 listener.onKeyUp(pygame.K_DOWN, mode_down)
+
 def on_left():
-    global camDir, camDirRads
-    camDirRads = camDirRads - 0.1
-    camDir = engine.mathdef.toVector(camDirRads)
+    global camera
+    # camDirRads = camDirRads - 0.1
+    # camDir = engine.mathdef.toVector(camDirRads)
+    camera.angle -= 0.1
 listener.onKeyHold(pygame.K_LEFT, on_left)
 def on_right():
-    global camDir, camDirRads
-    camDirRads = camDirRads + 0.1
-    camDir = engine.mathdef.toVector(camDirRads)
+    global camera
+    # camDirRads = camDirRads + 0.1
+    # camDir = engine.mathdef.toVector(camDirRads)
+    camera.angle += 0.1
 listener.onKeyHold(pygame.K_RIGHT, on_right)
 def on_a():
-    global camPoint
-    camPoint[0] = camPoint[0] + math.sin(camDirRads)
-    camPoint[1] = camPoint[1] - math.cos(camDirRads)
+    global camera
+    camera.worldX += math.sin(camera.angle)
+    camera.worldY -= math.cos(camera.angle)
+    # camPoint[0] = camPoint[0] + math.sin(camDirRads)
+    # camPoint[1] = camPoint[1] - math.cos(camDirRads)
 listener.onKeyHold(pygame.K_a, on_a)
 def on_d():
-    global camPoint
-    camPoint[0] = camPoint[0] - math.sin(camDirRads)
-    camPoint[1] = camPoint[1] + math.cos(camDirRads)
+    global camera
+    camera.worldX -= math.sin(camera.angle)
+    camera.worldY += math.cos(camera.angle)
+    # camPoint[0] = camPoint[0] - math.sin(camDirRads)
+    # camPoint[1] = camPoint[1] + math.cos(camDirRads)
 listener.onKeyHold(pygame.K_d, on_d)
 def on_w():
-    global camPoint
-    camPoint[0] = camPoint[0] + math.cos(camDirRads)
-    camPoint[1] = camPoint[1] + math.sin(camDirRads)
+    global camera
+    camera.worldX += math.cos(camera.angle)
+    camera.worldY += math.sin(camera.angle)
+    # camPoint[0] = camPoint[0] + math.cos(camDirRads)
+    # camPoint[1] = camPoint[1] + math.sin(camDirRads)
 listener.onKeyHold(pygame.K_w, on_w)
 def on_s():
-    global camPoint
-    camPoint[0] = camPoint[0] - math.cos(camDirRads)
-    camPoint[1] = camPoint[1] - math.sin(camDirRads)
+    global camera
+    camera.worldX -= math.cos(camera.angle)
+    camera.worldY -= math.sin(camera.angle)
+    # camPoint[0] = camPoint[0] - math.cos(camDirRads)
+    # camPoint[1] = camPoint[1] - math.sin(camDirRads)
 listener.onKeyHold(pygame.K_s, on_s)
 
 fpvpX = 500
@@ -214,130 +230,63 @@ while True:
         solidBsp.drawFaces(display, mx, my)
 
 
-
-
-    # BISQWIT
-    
     wall = [ [wallTest.start[0], wallTest.start[1]], [wallTest.end[0], wallTest.end[1]] ]
-    px = camPoint[0]
-    py = camPoint[1]
-    angle = camDirRads
     angleLength = 10
 
-    # TOP DOWN
+    # ========================
+    # Top Down Perspective
     
     # Render frame
     display.drawLines(tdBounds, (150, 0, 150), 2, True)
-
     # Render wall
     tdWall = inBoundLine(wall, tdBounds)
     display.drawLine(tdWall, (255, 50, 255), 2)
-
     # Render player angle
-    dir = [[px, py], [px + math.cos(angle) * angleLength, py + math.sin(angle) * angleLength]]
+    dir = [[camera.worldX, camera.worldY], [camera.worldX + math.cos(camera.angle) * angleLength, camera.worldY + math.sin(camera.angle) * angleLength]]
     tdDir = inBoundLine(dir, tdBounds)
     display.drawLine(tdDir, (255, 100, 255), 1)
-
     # Render player pos
-    tdCamPoint = inBoundPoint(camPoint, tdBounds)
+    tdCamPoint = inBoundPoint([camera.worldX, camera.worldY], tdBounds)
     display.drawPoint(tdCamPoint, (255, 255, 255), 2)
 
-
-    # PROJECTED
+    # ========================
+    # Transformed Perspected
 
     # Render frame
     display.drawLines(pjBounds, (150, 150, 0), 2, True)
-
     # Transform vertices relative to player
-    tx1 = wall[0][0] - px
-    ty1 = wall[0][1] - py
-    tx2 = wall[1][0] - px
-    ty2 = wall[1][1] - py
-
-    # Rotate them around the players view
-    tz1 = tx1 * math.cos(angle) + ty1 * math.sin(angle)
-    tz2 = tx2 * math.cos(angle) + ty2 * math.sin(angle)
-    tx1 = tx1 * math.sin(angle) - ty1 * math.cos(angle)
-    tx2 = tx2 * math.sin(angle) - ty2 * math.cos(angle)
-
+    (tx1, ty1, tz1, tx2, ty2, tz2) = camera.transformWall(wallTest)
     # Render wall
     pjWall = [[50 - tx1, 50 - tz1], [50 - tx2, 50 - tz2]]
     pjWall = inBoundLine(pjWall, pjBounds)
     display.drawLine(pjWall, (255, 255, 50), 2)
-
     # Render player angle
     pjDir = [[50, 50], [50, 50 - angleLength]]
     pjDir = inBoundLine(pjDir, pjBounds)
     display.drawLine(pjDir, (255, 255, 100), 1)
-
     # Render player pos
     pjCamPoint = [50, 50]
     pjCamPoint = inBoundPoint(pjCamPoint, pjBounds)
     display.drawPoint(pjCamPoint, (255, 255, 255), 2)
+    
 
-
-    # PERSPECTIVE TRANSFORMED
-
-    # Clip
-    # determine clipping, if both z's < 0 its totally behind
-	# if only 1 is negative it can be clipped
-    if tz1 > 0 or tz2 > 0:
-        # if line crosses the players view plane clip it
-        # i think the last two are set by refs
-        i1 = intersect(tx1, tz1, tx2, tz2, -0.0001, 0.0001, -20, 5)
-        ix1 = i1[0]
-        iz1 = i1[1]
-        i2 = intersect(tx1, tz1, tx2, tz2, 0.0001, 0.0001, 20, 5)
-        ix2 = i2[0]
-        iz2 = i2[1]
-        if tz1 <= 0:
-            if iz1 > 0:
-                tx1 = ix1
-                tz1 = iz1
-            else:
-                tx1 = ix2
-                tz1 = iz2
-        if tz2 <= 0:
-            if iz1 > 0:
-                tx2 = ix1
-                tz2 = iz1
-            else:
-                tx2 = ix2
-                tz2 = iz2
+    # ========================
+    # FPS Perspective
 
     # Render frame
     display.drawLines(fpBounds, (0, 150, 150), 2, True)
-
-    if (tz1 > 0 and tz2 > 0):
-
-        # Transform
-        x1 = -tx1 * 16 / tz1
-        y1a = -50 / tz1
-        y1b = 50 / tz1
-        x2 = -tx2 * 16 / tz2
-        y2a = -50 / tz2
-        y2b = 50 / tz2
-
-        # Render
-        topLine = [[50 + x1, 50 + y1a], [50 + x2, 50 + y2a]]
-        bottomLine = [[50 + x1, 50 + y1b], [50 + x2, 50 + y2b]]
-        leftLine = [[50 + x1, 50 + y1a], [50 + x1, 50 + y1b]]
-        rightLine = [[50 + x2, 50 + y2a], [50 + x2, 50 + y2b]]
-        
+    # Render Projection
+    topLine, rightLine, bottomLine, leftLine = camera.projectWall(wallTest)
+    if topLine is not None:
         fpTopLine = inBoundLine(topLine, fpBounds)
+        fpRightLine = inBoundLine(rightLine, fpBounds)
         fpBottomLine = inBoundLine(bottomLine, fpBounds)
         fpLeftLine = inBoundLine(leftLine, fpBounds)
-        fpRightLine = inBoundLine(rightLine, fpBounds)
         display.drawLine(fpTopLine, (0, 255, 255), 2)
         display.drawLine(fpBottomLine, (0, 255, 255), 2)
         display.drawLine(fpLeftLine, (0, 255, 255), 2)
         display.drawLine(fpRightLine, (0, 255, 255), 2)
-
-
-
-
-
-
+    
     # draw our position information
     text = font.render("{}, {}".format(mx, my), 1, (50, 50, 50))
     textpos = text.get_rect(centerx = display.width / 2, centery = display.height/2)
