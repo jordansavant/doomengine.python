7068cfc76780eabc44bde9b5f14ae32ee1d76dc4
Jordan Savant <md5madman@gmail.com>
2019-12-14 09:54:32 -0600

    simple loaders for string and integer

--- engine_diy/wad_loader.py
+++ engine_diy/wad_loader.py
@@ -47,67 +47,36 @@ class WADLoader(object):
         #self.header = self.f.read(4)
         #self.dircount = self.f.read(4)
         #self.diroffset = self.f.read(4)
-        self.type = self.read4bytes(0)
+        #self.type = self.read4bytes(0)
         self.dircount = self.read4bytes(4)
         self.diroffset = self.read4bytes(8)
+        self.type = self.loadString(0, 4)
+        self.dircount = self.loadInt(4, 4)
+        self.diroffset = self.loadInt(8, 4)
 
         # convert to string
-        self.typestr = self.asstr(self.type)
-        self.dircounti = self.asint(self.dircount)
+        #self.typestr = self.asstr(self.type)
+        #self.dircounti = self.asint(self.dircount)
 
-        print(self.typestr, self.dircounti)
-        print(self.get4s(0), self.get4i(4))
+        #print(self.typestr, self.dircounti)
+        #print(self.get4s(0), self.get4i(4))
 
-    def asstr(self, arr):
-        ss = ''
-        for i, c in enumerate(arr):
-            ss += chr(c)
-        return ss
-
-    def asint(self, arr):
-        return sum(arr[i] << (i * 8) for i in range(len(arr)))
-        #return (arr[3] << 24) + (arr[2] << 16) + (arr[1] << 8) + arr[0]
-
-    def get4s(self, offset):
+    def loadString(self, offset, length):
         self.f.seek(offset)
-        ss = [0,0,0,0]
         sss = ''
-        for i in range(0, 4):
-            ss[i] = struct.unpack('<c', self.f.read(1))[0]
-            sss += str(ss[i], 'utf-8')
+        for i in range(0, length):
+            c = struct.unpack('<c', self.f.read(1))[0]
+            sss += str(c, 'utf-8')
         return sss
-        return ss
 
-    def get4i(self, offset):
+    def loadInt(self, offset, length):
         self.f.seek(offset)
-        big = self.f.read(4) # big endian
-        lil = struct.unpack_from('<I', big)
-        return lil[0]
-
-    # TODO load file into memory for faster access
-    def read2bytes(self, offset):
-        # get raw binary and convert from big-ending
-        # to little-endian
-        self.f.seek(offset)
-        a = self.f.read(1)
-        b = self.f.read(1)
-        a = int.from_bytes(a, 'big')
-        b = int.from_bytes(b, 'big')
-        return [a, b]
-        #return (b << 8) | a
-
-    def read4bytes(self, offset):
-        self.f.seek(offset)
-        a = self.f.read(1)
-        b = self.f.read(1)
-        c = self.f.read(1)
-        d = self.f.read(1)
-        a = int.from_bytes(a, 'big')
-        b = int.from_bytes(b, 'big')
-        c = int.from_bytes(c, 'big')
-        d = int.from_bytes(d, 'big')
-        return [a, b, c, d]
-        #return (d << 24) | (c << 16) | (b << 8) | a
+        ii = 0
+        for i in range(0, length):
+            buf = self.f.read(1)
+            byte = int.from_bytes(buf, 'big')
+            ii += (byte << (i * 8))
+        return ii
 
     def __str__(self):
         return "\
@@ -116,5 +85,5 @@ WADLoader\n\
  TYPE: {}\n\
  DIRS: {}\n\
  DOFFSET: {}\
- ".format(self.wadpath, self.typestr, self.dircount, self.diroffset)
+ ".format(self.wadpath, self.type, self.dircount, self.diroffset)
 
